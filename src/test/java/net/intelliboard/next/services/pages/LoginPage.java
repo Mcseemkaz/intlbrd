6012bfca7d06e5354f87db95a7df3788c2695af4
Maksym Ovsiuk <movsiuk@intelliboard.net>
2022-03-06 10:11:52 +0200

    fix new login flow

--- src/test/java/net/intelliboard/next/services/pages/LoginPage.java
+++ src/test/java/net/intelliboard/next/services/pages/LoginPage.java
@@ -1,6 +1,7 @@
 package net.intelliboard.next.services.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import net.intelliboard.next.services.pages.signup.SignUpFormPage;
 import net.intelliboard.next.services.pages.signup.SignUpInvitePage;
 
 import static com.codeborne.selenide.Selenide.$x;
@@ -11,9 +12,9 @@ public class LoginPage {
     public SelenideElement passwordField = $x("//input[@id=\"login-password\"]");
     public SelenideElement buttonSubmit = $x("//button[@type=\"submit\"]");
 
-    public SignUpInvitePage goToRegistartion() {
+    public SignUpFormPage goToRegistartion() {
         $x("//a[contains (@class,'signup')]").click();
-        return SignUpInvitePage.init();
+        return SignUpFormPage.init();
     }
 
 }
--- src/test/java/net/intelliboard/next/services/pages/signup/SignUpInvitePage.java
+++ src/test/java/net/intelliboard/next/services/pages/signup/SignUpInvitePage.java
@@ -13,7 +13,7 @@ public class SignUpInvitePage {
     public static SignUpInvitePage init(){
         $x("//div[contains (@class,'signup-page')]").shouldBe(Condition.visible);
         String currentURL = WebDriverRunner.getWebDriver().getCurrentUrl();
-        assertThat(currentURL).isEqualTo(IBNextURLs.SIGNUP_INVITATION_PAGE);
+        assertThat(currentURL).isEqualTo(IBNextURLs.SIGNUP_PAGE);
         return new SignUpInvitePage();
     }
 
