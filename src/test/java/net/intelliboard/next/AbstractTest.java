fcc41f489c486a0917c5084431822d85b480a7e6
Maksym Ovsiuk <movsiuk@intelliboard.net>
2022-05-13 14:52:24 +0300

    Added logger to stdout for test execution

--- src/test/java/net/intelliboard/next/AbstractTest.java
+++ src/test/java/net/intelliboard/next/AbstractTest.java
@@ -3,20 +3,28 @@ package net.intelliboard.next;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import net.intelliboard.next.services.ConsoleColors;
 import net.intelliboard.next.services.PropertiesGetValue;
+import net.intelliboard.next.services.TestsWatcherImpl;
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.IOException;
 
-
+@ExtendWith(TestsWatcherImpl.class)
 public abstract class AbstractTest {
 
     protected static PropertiesGetValue propertiesGetValue = new PropertiesGetValue();
-
+    private static final Logger LOGGER = Logger.getLogger("AbstractTest");
     @BeforeEach
-    public void setUp() throws IOException {
+    public void setUp(TestInfo testInfo) throws IOException {
+
+        LOGGER.info(String.format("Test started : %s", testInfo.getDisplayName()));
 
         switch (propertiesGetValue.getPropertyValue("browser")) {
 
@@ -45,13 +53,18 @@ public abstract class AbstractTest {
         WebDriverRunner.clearBrowserCache();
         Configuration.browserSize = "1600x1200";
         Configuration.timeout = 20000;
-
     }
 
     @AfterEach
-    public void tearDown() {
+    public void tearDown(TestInfo testInfo) {
+        LOGGER.info(String.format("Test finished : %s", testInfo.getDisplayName()));
         WebDriverRunner.driver().close();
     }
 
-
+    @AfterAll
+    public static void rumpUp(){
+        System.out.println(String.format("--------------------- %sTEST RESULTS%s -----------------------------", ConsoleColors.BLACK_BACKGROUND,ConsoleColors.RESET));
+        TestsWatcherImpl.testResultsStatus.forEach((k,v) -> System.out.println(String.format("| %s | %s |", k, v)));
+        System.out.println("----------------------------------------------------------------");
+    }
 }
