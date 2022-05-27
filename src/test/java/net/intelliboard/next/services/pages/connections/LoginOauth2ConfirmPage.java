98e1809bcec76fddfcd27affa1efa0ea3b5a1000
Maksym Ovsiuk <movsiuk@intelliboard.net>
2022-05-27 08:23:52 +0300

    AT-5: Added more timeout for Canvas flow

--- src/test/java/net/intelliboard/next/services/pages/connections/LoginOauth2ConfirmPage.java
+++ src/test/java/net/intelliboard/next/services/pages/connections/LoginOauth2ConfirmPage.java
@@ -5,6 +5,8 @@ import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import net.intelliboard.next.services.IBNextURLs;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selenide.$x;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +14,8 @@ public class LoginOauth2ConfirmPage {
     private SelenideElement inputAuthorizeField = $x("//input[@type=\"submit\"]");
 
     public static LoginOauth2ConfirmPage init() {
-        $x("//div[contains (@class,'ic-Login-confirmation__content')]").shouldBe(Condition.visible);
+        $x("//div[contains (@class,'ic-Login-confirmation__content')]").shouldBe(Condition.visible,
+                Duration.ofSeconds(100));
         String currentURL = WebDriverRunner.getWebDriver().getCurrentUrl();
         assertThat(currentURL).isEqualTo(IBNextURLs.LOGIN_OAUTH2_CONFIRM_PAGE);
         return new LoginOauth2ConfirmPage();
