eb5ae7b3c7a428dcf344343619214498a0331ce1
Maksym Ovsiuk <movsiuk@intelliboard.net>
2022-09-12 04:15:10 +0300

    AT-74: fix SyncUser

--- src/test/java/net/intelliboard/next/services/pages/IBUsers/IBUsersSyncPage.java
+++ src/test/java/net/intelliboard/next/services/pages/IBUsers/IBUsersSyncPage.java
@@ -15,8 +15,8 @@ public class IBUsersSyncPage {
             $x("//div[contains(@class,'card-body')]//div[@name='lms_users_ids']//button[@class='tree-choice']");
     private SelenideElement RolesDropdown =
             $x("//div[contains(@class,'card-body')]//div[@name='role_id']//div[contains (@class,'intelli-dropdown')]");
-    private SelenideElement LMSDropdown =
-            $x("//div[contains(@class,'card-body')]//div[@name='lms_roles']//div[contains (@class,'intelli-dropdown')]");
+    private SelenideElement ConnectionRolesDropdown =
+            $x("//div[contains(@class,'card-body')]//div[@name='connection_roles']//div[contains (@class,'intelli-dropdown')]");
 
 
     public static IBUsersSyncPage init() {
@@ -49,7 +49,7 @@ public class IBUsersSyncPage {
 
     public IBUsersSyncPage selectLMSRole(String name) {
 
-        LMSDropdown.click();
+        ConnectionRolesDropdown.click();
 
         $x("//div[contains(@class,'card-body')]//div[@name='lms_roles']//div[contains (@class,'intelli-dropdown')]//li//strong[text()='" + name + "']")
                 .click();
@@ -59,10 +59,10 @@ public class IBUsersSyncPage {
     }
 
     public IBUsersSyncPage selectLMSRole() {
-       LMSDropdown.click();
+       ConnectionRolesDropdown.click();
 
-        $x("(//div[contains(@class,'card-body')]//div[@name='lms_roles']//div[contains (@class,'intelli-dropdown')]//li//strong)[1]")
-                .click();
+//        $x("(//div[contains(@class,'card-body')]//div[@name='lms_roles']//div[contains (@class,'intelli-dropdown')]//li//strong)[1]")
+//                .click();
         $x("//button[@type='submit']")
                 .click();
         return this;
