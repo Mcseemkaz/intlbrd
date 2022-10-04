3e63a38a4b03547bbbf906c27450ea4c8557dce8
Maksym Ovsiuk <movsiuk@intelliboard.net>
2022-10-04 17:06:39 +0300

    AT-94: Added screenshot in Allure report when test fail

--- src/test/java/net/intelliboard/next/AbstractTest.java
+++ src/test/java/net/intelliboard/next/AbstractTest.java
@@ -1,24 +1,25 @@
 package net.intelliboard.next;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import net.intelliboard.next.services.ConsoleColors;
 import net.intelliboard.next.services.PropertiesGetValue;
 import net.intelliboard.next.services.login.LoginCookieHandler;
 import net.intelliboard.next.services.looger.TestsWatcherImpl;
 import net.intelliboard.next.services.webdriver.WebDriverService;
-import org.apache.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @ExtendWith(TestsWatcherImpl.class)
+
 public abstract class AbstractTest {
 
+    static Logger logger = LoggerFactory.getLogger(AbstractTest.class);
     public static PropertiesGetValue propertiesGetValue = new PropertiesGetValue();
     public static Long WAIT_TIMEOUT_LONG;
 
@@ -30,35 +31,27 @@ public abstract class AbstractTest {
         }
     }
 
-    private static final Logger LOGGER = Logger.getLogger("AbstractTest");
-
     @BeforeEach
     public void setUp(TestInfo testInfo) throws IOException {
-
-        System.out.println(String.format("Test started : %s", testInfo.getDisplayName()));
-//        LOGGER.info(String.format("Test started : %s", testInfo.getDisplayName()));
-
+        logger.info(String.format("Test started : %s", testInfo.getDisplayName()));
         WebDriverService driver = new WebDriverService();
-        driver.initWebDriver(propertiesGetValue.getPropertyValue("browser"),
-                Integer.valueOf(propertiesGetValue.getPropertyValue("timeout")),
+        driver.initWebDriver(
+                propertiesGetValue.getPropertyValue("browser"),
+                Integer.parseInt(propertiesGetValue.getPropertyValue("timeout")),
                 propertiesGetValue.getPropertyValue("resolution"), testInfo);
         LoginCookieHandler.run();
-
     }
 
     @AfterEach
     public void tearDown(TestInfo testInfo) {
-        System.out.println(String.format("Test finished : %s", testInfo.getDisplayName()));
-//        LOGGER.info(String.format("Test finished : %s", testInfo.getDisplayName()));
-        Selenide.clearBrowserCookies();
-        Selenide.clearBrowserLocalStorage();
-        WebDriverRunner.driver().close();
+        logger.info(String.format("Test finished : %s", testInfo.getDisplayName()));
     }
 
     @AfterAll
     public static void wrapUp() {
-        System.out.println(String.format("--------------------- %sTEST RESULTS%s -----------------------------", ConsoleColors.BLACK_BACKGROUND, ConsoleColors.RESET));
-        TestsWatcherImpl.testResultsStatus.forEach((k, v) -> System.out.println(String.format("| %s | %s |", k, v)));
+        System.out.println(String.format("--------------------- %sTEST RESULTS%s -----------------------------%n",
+                ConsoleColors.BLACK_BACKGROUND, ConsoleColors.RESET));
+        TestsWatcherImpl.testResultsStatus.forEach((k, v) -> System.out.printf("| %s | %s |%n", k, v));
         System.out.println("----------------------------------------------------------------");
         LoginCookieHandler.setIsCookieSet(false);
     }
--- src/test/java/net/intelliboard/next/services/looger/TestsWatcherImpl.java
+++ src/test/java/net/intelliboard/next/services/looger/TestsWatcherImpl.java
@@ -1,44 +1,71 @@
 package net.intelliboard.next.services.looger;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import net.intelliboard.next.AbstractTest;
 import net.intelliboard.next.services.ConsoleColors;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.OutputType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
+import static io.qameta.allure.Allure.addAttachment;
 
 public class TestsWatcherImpl implements TestWatcher {
 
     public static Map<String, TestResultStatus> testResultsStatus = new HashMap<>();
+    Logger loggerWatcher = LoggerFactory.getLogger(AbstractTest.class);
 
     public enum TestResultStatus {
-        SUCCESSFUL, ABORTED, FAILED, DISABLED;
+        SUCCESSFUL, ABORTED, FAILED, DISABLED
     }
 
     @Override
     public void testSuccessful(ExtensionContext context) {
-        System.out.println(String.format("Test %sPASSED%s: %s", ConsoleColors.GREEN, ConsoleColors.RESET, context.getDisplayName()));
+        loggerWatcher.info(String.format("Test %sPASSED%s: %s", ConsoleColors.GREEN, ConsoleColors.RESET, context.getDisplayName()));
         testResultsStatus.put(context.getDisplayName(), TestResultStatus.SUCCESSFUL);
+        shutDownSystem();
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
-        System.out.println(String.format("Test %sFAILED%s: %s \n %s", ConsoleColors.RED_BACKGROUND, ConsoleColors.RESET,
+        loggerWatcher.error(String.format("Test %sFAILED%s: %s \n %s", ConsoleColors.RED_BACKGROUND, ConsoleColors.RESET,
                 context.getDisplayName(), cause));
         testResultsStatus.put(context.getDisplayName(), TestResultStatus.FAILED);
+        try {
+            File screenshotAs = Selenide.screenshot(OutputType.FILE);
+            addAttachment("Screenshot", FileUtils.openInputStream(screenshotAs));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        shutDownSystem();
     }
 
     @Override
     public void testAborted(ExtensionContext context, Throwable cause) {
-        System.out.println(String.format("Test %sABORTED%s: %s \n %s", ConsoleColors.YELLOW, ConsoleColors.RESET,
+        loggerWatcher.warn(String.format("Test %sABORTED%s: %s \n %s", ConsoleColors.YELLOW, ConsoleColors.RESET,
                 context.getDisplayName(), cause));
         testResultsStatus.put(context.getDisplayName(), TestResultStatus.ABORTED);
+        shutDownSystem();
     }
 
     @Override
     public void testDisabled(ExtensionContext context, Optional<String> reason) {
-        System.out.println(String.format("Test %sFAILED%s: %s \n %s", ConsoleColors.RED_BACKGROUND, ConsoleColors.RESET, context.getDisplayName(),
+        loggerWatcher.warn(String.format("Test %sDISABLED%s: %s \n %s", ConsoleColors.RED_BACKGROUND, ConsoleColors.RESET, context.getDisplayName(),
                 reason.orElse("No reason")));
         testResultsStatus.put(context.getDisplayName(), TestResultStatus.DISABLED);
+        shutDownSystem();
+    }
+
+    private void shutDownSystem() {
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+        WebDriverRunner.driver().close();
     }
 }
