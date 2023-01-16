500b49d771f729af570573a7a54a0143ff3f20b8
Maksym Ovsiuk <movsiuk@intelliboard.net>
2023-01-16 07:44:32 +0200

    AT-147: Add test SP-T94

--- src/test/java/net/intelliboard/next/tests/in_form/InFromTest.java
+++ src/test/java/net/intelliboard/next/tests/in_form/InFromTest.java
@@ -4,11 +4,13 @@ import io.qameta.allure.Feature;
 import net.intelliboard.next.IBNextAbstractTest;
 import net.intelliboard.next.services.IBNextURLs;
 import net.intelliboard.next.services.helpers.DataGenerator;
+import net.intelliboard.next.services.pages.header.HeaderObject;
 import net.intelliboard.next.services.pages.inform.InFormColumnType;
 import net.intelliboard.next.services.pages.inform.InFormPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -23,9 +25,9 @@ public class InFromTest extends IBNextAbstractTest {
     @EnumSource(value = InFormColumnType.class)
     @Tags(value = {@Tag("smoke"), @Tag("normal"), @Tag("SP-T89")})
     @DisplayName("SP-T89: Create Inform table")
-    public void createInformTableTest(InFormColumnType value) throws InterruptedException {
+    void createInformTableTest(InFormColumnType value) throws InterruptedException {
 
-        String tableName = DataGenerator.getRandomString() + " TestAQA";
+        String tableName = "SP-T89_" + value + DataGenerator.getRandomString();
 
         open(IBNextURLs.INFORM_LIST_PAGE);
 
@@ -38,11 +40,30 @@ public class InFromTest extends IBNextAbstractTest {
 
         InFormPage inFormPage = InFormPage.init();
         assertThat(inFormPage.isTableExist(tableName))
-                .isTrue()
-                .as(String.format("Table : %s ", tableName));
+                .withFailMessage("Table :%s is not existed", tableName)
+                .isTrue();
 
         inFormPage
                 .searchInfoTable(tableName)
                 .deleteTable(tableName);
+    }
+
+    //TODO [MO] DropDown Issue
+    @Test
+    @Tags(value = {@Tag("smoke"), @Tag("SP-T94")})
+    @DisplayName("SP-T94: Create Inform form")
+    void testCreateInFormForm() {
+
+        String inFormTable = "Automation InFormTable";
+        String inFormName = "SP-94_" + DataGenerator.getRandomString();
+
+        HeaderObject
+                .init()
+                .createInFormForm()
+                .selectTable(inFormTable)
+                .setInFormName(inFormName)
+                .addColumn("Text")
+                .addColumn("Number")
+                .saveForm();
     }
 }
