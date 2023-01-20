fe603729f090fcd52bc6a9ec2607fe7f66480cdc
Maksym Ovsiuk <movsiuk@intelliboard.net>
2023-01-20 09:18:51 +0200

    AT-153: Added SP-T1093 SP-T1092

--- src/test/java/net/intelliboard/next/tests/core/ibusers/orgrole/IBUserOrgRolesTest.java
+++ src/test/java/net/intelliboard/next/tests/core/ibusers/orgrole/IBUserOrgRolesTest.java
@@ -7,6 +7,7 @@ import net.intelliboard.next.services.IBNextURLs;
 import net.intelliboard.next.services.helpers.DataGenerator;
 import net.intelliboard.next.services.pages.IBUsers.IBUsersPage;
 import net.intelliboard.next.services.pages.IBUsers.OrgRoleStatusEnum;
+import net.intelliboard.next.services.pages.IBUsers.OrgRolesMainPage;
 import net.intelliboard.next.services.pages.connections.ConnectionsTypeEnum;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -14,6 +15,8 @@ import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("Organizational_Roles")
 @Feature("Organizational roles")
@@ -36,7 +39,71 @@ class IBUserOrgRolesTest extends IBNextAbstractTest {
                 .fillInOrgRoleName(orgRoleName)
                 .setStatus(OrgRoleStatusEnum.ACTIVE)
                 .selectConnection(ConnectionsTypeEnum.CANVAS.defaultName)
-                .submitUserCreateForm()
+                .saveOrgRole()
                 .deleteOrgRole(orgRoleName);
+    }
+
+    @Test
+    @Tags(value = {@Tag("normal"), @Tag("SP-T1093")})
+    @DisplayName("SP-T1093: Delete the Organizational role")
+    @Description("Verify that created role can be successful deleted")
+    void testDeleteOrganizationRole() {
+
+        String orgRoleName = "SP-T1093_" + DataGenerator.getRandomString();
+
+        open(IBNextURLs.USERS_PAGE);
+
+        IBUsersPage
+                .init()
+                .openOrgRolesPage()
+                .openAddRole()
+                .fillInOrgRoleName(orgRoleName)
+                .setStatus(OrgRoleStatusEnum.ACTIVE)
+                .selectConnection(ConnectionsTypeEnum.CANVAS.defaultName)
+                .saveOrgRole()
+                .deleteOrgRole(orgRoleName);
+
+        assertFalse(OrgRolesMainPage.init().isOrgRoleExist(orgRoleName));
+    }
+
+    @Test
+    @Tags(value = {@Tag("normal"), @Tag("SP-T1092")})
+    @DisplayName("SP-T1092: Edit an Organizational role")
+    @Description("Verify that the edit Organizational role work correctly")
+    void testEditOrganizationRole() {
+
+        String orgRoleName = "SP-T1092_" + DataGenerator.getRandomString();
+        String orgRoleNameUpdate = orgRoleName + "_UPD";
+
+        open(IBNextURLs.USERS_PAGE);
+
+        IBUsersPage
+                .init()
+                .openOrgRolesPage()
+                .openAddRole()
+                .fillInOrgRoleName(orgRoleName)
+                .setStatus(OrgRoleStatusEnum.ACTIVE)
+                .selectConnection(ConnectionsTypeEnum.CANVAS.defaultName)
+                .saveOrgRole();
+
+        assertTrue(OrgRolesMainPage
+                .init()
+                .isOrgRoleExist(orgRoleName));
+
+        OrgRolesMainPage
+                .init()
+                .editOrgRole(orgRoleName)
+                .fillInOrgRoleName(orgRoleNameUpdate)
+                .saveOrgRole();
+
+        assertTrue(OrgRolesMainPage
+                .init()
+                .isOrgRoleExist(orgRoleNameUpdate));
+
+        OrgRolesMainPage
+                .init()
+                .deleteOrgRole(orgRoleNameUpdate);
+
+        assertFalse(OrgRolesMainPage.init().isOrgRoleExist(orgRoleNameUpdate));
     }
 }
