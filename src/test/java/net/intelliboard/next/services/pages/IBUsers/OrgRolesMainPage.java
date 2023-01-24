94e77b8c44b0f01418d1c4ced0c42f103cf200b6
Maksym Ovsiuk <movsiuk@intelliboard.net>
2023-01-24 07:24:16 +0200

    AT-155: Add additional waiter for Org Role Creation

--- src/test/java/net/intelliboard/next/services/pages/IBUsers/OrgRolesMainPage.java
+++ src/test/java/net/intelliboard/next/services/pages/IBUsers/OrgRolesMainPage.java
@@ -1,13 +1,18 @@
 package net.intelliboard.next.services.pages.IBUsers;
 
 import com.codeborne.selenide.Condition;
+import net.intelliboard.next.IBNextAbstractTest;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class OrgRolesMainPage {
+    static IBNextAbstractTest ibNextAbstractTest = new IBNextAbstractTest();
+
     public static OrgRolesMainPage init() {
+        ibNextAbstractTest.waitForPageLoaded();
         $x("//main//h1[contains (text(),'Roles')]")
                 .shouldBe(Condition.visible);
+
         return new OrgRolesMainPage();
     }
 
