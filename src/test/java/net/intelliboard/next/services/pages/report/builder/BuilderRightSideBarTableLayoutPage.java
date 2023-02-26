3ccefed7772369b48452e1ca30604e8b87fbc4ee
Maksym Ovsiuk <movsiuk@intelliboard.net>
2023-02-26 06:58:24 +0200

    AT-174: fix SP-T692

--- src/test/java/net/intelliboard/next/services/pages/report/builder/BuilderRightSideBarTableLayoutPage.java
+++ src/test/java/net/intelliboard/next/services/pages/report/builder/BuilderRightSideBarTableLayoutPage.java
@@ -3,28 +3,34 @@ package net.intelliboard.next.services.pages.report.builder;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static net.intelliboard.next.AbstractTest.SLEEP_TIMEOUT_SHORT;
 
 public class BuilderRightSideBarTableLayoutPage {
 
-    private SelenideElement displayElementsBlock = $x("//section[@class='optional-block']");
+    private final SelenideElement displayElementsBlock = $x("//section[@class='optional-block']");
 
     public static BuilderRightSideBarTableLayoutPage init() {
         return new BuilderRightSideBarTableLayoutPage();
     }
-
+    @Step("Open 'Add Display Element' Block")
     private BuilderRightSideBarTableLayoutPage openDisplayElementsList() {
         if (!displayElementsBlock.isDisplayed()) {
-            $x("//a[contains (text(),'Add Field')]").click();
+            $x("//a[contains (text(),'Add Field')]")
+                    .click();
             displayElementsBlock.shouldBe(Condition.visible, Duration.ofSeconds(30));
         }
         return this;
     }
 
-    public BuilderRightSideBarTableLayoutPage addDisplayElement(ReportBuilderDisplayElementsMainEnum menuElement, ReportBuilderDisplayElementEnum displayElement) {
+    @Step("Add display element")
+    public BuilderRightSideBarTableLayoutPage addDisplayElement(
+            ReportBuilderDisplayElementsMainEnum menuElement,
+            ReportBuilderDisplayElementEnum displayElement) {
         openDisplayElementsList();
         BuilderRightSideElementsManagerPage
                 .init()
@@ -37,14 +43,16 @@ public class BuilderRightSideBarTableLayoutPage {
         return this;
     }
 
-    public BuilderReportFormulaElement openAddFormulaEditor(){
+    @Step("Open Formula Editor")
+    public BuilderReportFormulaElement openAddFormulaEditor() {
         openDisplayElementsList();
-        Selenide.sleep(3000);
+        Selenide.sleep(SLEEP_TIMEOUT_SHORT);
         openFormulaEditor();
         return BuilderReportFormulaElement.init();
     }
 
-    private void openFormulaEditor(){
-        $x("//a[@class='formula-link']").click();
+    private void openFormulaEditor() {
+        Selenide
+                .executeJavaScript("document.getElementsByClassName('formula-link')[0].click();");
     }
 }
