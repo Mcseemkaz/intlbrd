490d3c3b403f745c794a7794a4c5380f15d51235
Maksym Ovsiuk <movsiuk@intelliboard.net>
2023-03-11 06:45:18 +0200

    AT-189: Add Auto Login for stage env

---
+++ src/test/java/net/intelliboard/next/services/login/AutoLoginService.java
@@ -0,0 +1,29 @@
+package net.intelliboard.next.services.login;
+
+import com.codeborne.selenide.Selenide;
+import net.intelliboard.next.services.IBNextURLs;
+import net.intelliboard.next.services.PropertiesGetValue;
+import net.intelliboard.next.services.pages.elements.spinners.PageSpinner;
+
+import java.io.IOException;
+
+public class AutoLoginService {
+
+    private static final String AUTO_LOGIN_TOKEN;
+    private static final String AUTO_LOGIN_EMAIL;
+
+    static {
+        try {
+            AUTO_LOGIN_TOKEN = new PropertiesGetValue().getPropertyValue("user_token");
+            AUTO_LOGIN_EMAIL = new PropertiesGetValue().getPropertyValue("user_login");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void autoLogin() {
+        Selenide.open(String.format(IBNextURLs.AUTO_LOGIN, AUTO_LOGIN_EMAIL, AUTO_LOGIN_TOKEN));
+        PageSpinner.waitPreloader();
+        PageSpinner.waitSpinner();
+    }
+}
