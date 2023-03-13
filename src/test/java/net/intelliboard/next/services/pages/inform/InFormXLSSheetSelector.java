d9fbe557fcff44908d0a25da0d664c60d6075233
Maksym Ovsiuk <movsiuk@intelliboard.net>
2023-03-13 15:56:24 +0200

    AT-185: Add sleep fix for PROD's SP-T890

--- src/test/java/net/intelliboard/next/services/pages/inform/InFormXLSSheetSelector.java
+++ src/test/java/net/intelliboard/next/services/pages/inform/InFormXLSSheetSelector.java
@@ -1,11 +1,13 @@
 package net.intelliboard.next.services.pages.inform;
 
 import com.codeborne.selenide.Condition;
+import net.intelliboard.next.services.pages.elements.spinners.PageSpinner;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class InFormXLSSheetSelector {
     public static InFormXLSSheetSelector init() {
+        PageSpinner.waitSpinner();
         $x("//select[@id='sheetName']")
                 .should(Condition.visible);
         return new InFormXLSSheetSelector();
